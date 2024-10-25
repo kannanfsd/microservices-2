bbb7256c6f4c23ccf45ecc3a53f5ba23ae2529b5
Kannan Rajendran <kannannglabs@gmail.com>
2024-10-25 20:22:29 +0530

    apply refresh config at runtime

--- section-6/v2-spring-cloud-config/loans/src/main/java/com/easyfund/dto/LoansContactDto.java
+++ section-6/v2-spring-cloud-config/loans/src/main/java/com/easyfund/dto/LoansContactDto.java
@@ -1,10 +1,18 @@
 package com.easyfund.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class LoansContactDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
